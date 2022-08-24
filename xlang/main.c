78f1af20f253c9c14e39ce1377b5dec3ef780f84
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-24 23:24:40 +0430

    test bytecode stream functions

--- xlang/main.c
+++ xlang/main.c
@@ -1,6 +1,12 @@
 #include "common.h"
+#include "bytecode.h"
+#include "logger.h"
 
 int main(int argc, const char* argv[])
 {
+    Bytecode bytecode;
+    init_bytecode(&bytecode);
+    write_bytecode(&bytecode, OP_RETURN);
+    free_bytecode(&bytecode);
     return 0;
 }
