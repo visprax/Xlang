00a43ac31e7f603b23aa34f5aea93bef87016dbf
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-24 20:38:27 +0430

    add formatted logger message

--- xlang/logger.c
+++ xlang/logger.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <time.h>
 
 #include "logger.h"
 
-void logger(const char* level, const char* message)
+void logger(const char* level, const char* message, ...)
 {
-    time_t now;
-    time(&now);
-    fprintf(stderr, "[%s] %8s: %s\n", ctime(&now), level, message);
+    time_t now = time(NULL);
+    struct tm* datetime = localtime(&now);
+    
+    char datetime_buff[64];
+    strftime(datetime_buff, sizeof datetime_buff, "%d-%b-%Y %X", datetime);
+    // variadic arguments
+    va_list args;
+    va_start(args, message);
+    fprintf(stderr, "[%s] %8s: ", datetime_buff, level);
+    vfprintf(stderr, message, args);
+    fprintf(stderr, "\n")
+    va_end(args);
 }
