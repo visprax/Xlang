6b2a8d493b86e231f12307b04cae8505abc9a69d
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-25 02:54:19 +0430

    test disassembler

--- xlang/main.c
+++ xlang/main.c
@@ -1,12 +1,17 @@
 #include "common.h"
-#include "bytecode.h"
 #include "logger.h"
+#include "bytecode.h"
+#include "debugger.h"
+
 
 int main(int argc, const char* argv[])
 {
     Bytecode bytecode;
     init_bytecode(&bytecode);
     write_bytecode(&bytecode, OP_RETURN);
+    
+    disassemble_bytecode(&bytecode, "Test Bytecode");
+
     free_bytecode(&bytecode);
     return 0;
 }
