dc901e475cceb43bb0cd8e3cba8c73f3818cd479
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-24 20:38:05 +0430

    add logger header file

--- xlang/logger.h
+++ xlang/logger.h
@@ -1,6 +1,9 @@
 #ifndef XLANG_LOGGER_H
 #define XLANG_LOGGER_H
 
-void logger(const char* level, const char* message);
+// maximum datetime buffer length
+#define MAX_DT_BUF_LEN 64
+
+void logger(const char* level, const char* message, ...);
 
 #endif // XLANG_LOGGER_H
