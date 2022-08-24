e1bda22870ce0287fefcd7fc5be06cdd1cba8bcf
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-24 20:39:08 +0430

    add debug info in case if allocation failure

--- xlang/memory.c
+++ xlang/memory.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 #include "memory.h"
+#include "logger.h"
 
 void* reallocate(void* pointer, size_t old_size, size_t new_size)
 {
@@ -11,6 +12,11 @@ void* reallocate(void* pointer, size_t old_size, size_t new_size)
     }
 
     void* result = realloc(pointer, new_size);
-    if (result == NULL) exit(1);
+    if (result == NULL)
+    {
+        logger("CRITICAL", "couldn't reallocate an array of new size: %d", new_size);
+        exit(EXIT_FAILURE);
+
+    }
     return result;
 }
