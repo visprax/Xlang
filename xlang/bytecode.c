1e4c6023c92c6692d9188fc35a06b6cd899c6c62
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-24 23:22:29 +0430

    add logging infor

--- xlang/bytecode.c
+++ xlang/bytecode.c
@@ -2,18 +2,30 @@
 
 #include "bytecode.h"
 #include "memory.h"
+#include "logger.h"
 
 void init_bytecode(Bytecode* bytecode)
 {
+    logger("INFO", "intializing bytecode stream");
     bytecode->size = 0;
     bytecode->capacity = 0;
     bytecode->code = NULL;
 }
 
+void free_bytecode(Bytecode* bytecode)
+{
+    logger("INFO", "freeing bytecode stream");
+    FREE_ARRAY(uint8_t, bytecode->code, bytecode->capacity);
+    // leave the bytecode in a well-defined state
+    init_bytecode(bytecode);
+}
+
 void write_bytecode(Bytecode* bytecode, uint8_t byte)
 {
+    logger("INFO", "writing bytecode stream");
     if (bytecode->capacity < bytecode->size+1)
     {
+        logger("DEBUG", "increasing bytecode array capacity");
         int old_capacity = bytecode->capacity;
         bytecode->capacity = GROW_CAPACITY(old_capacity);
         bytecode->code = GROW_ARRAY(uint8_t, bytecode->code, old_capacity, bytecode->capacity);
