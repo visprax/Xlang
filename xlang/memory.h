4cf709a35c67d04327152c517d328fb29d0dfa3e
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-24 23:24:15 +0430

    add FREE_ARRAY macro

--- xlang/memory.h
+++ xlang/memory.h
@@ -19,7 +19,11 @@
  * `void*` back to a pointer of the right type.
  */
 #define GROW_ARRAY(type, pointer, old_size, new_size) \
-    (type*)reallocate(pointer, sizeof(type)*old_size, sizeof(type)*new_size)
+    (type*)reallocate(pointer, sizeof(type)*(old_size), sizeof(type)*(new_size))
+
+#define FREE_ARRAY(type, pointer, old_size) \
+    reallocate(pointer, sizeof(type)*(old_size), 0)
+
 
 void* reallocate(void* pointer, size_t old_size, size_t new_size);
 
