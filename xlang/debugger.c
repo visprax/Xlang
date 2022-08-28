b866f3a0e9c133a8ed79ca5fc2d896e2420b67a1
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-28 22:37:05 +0430

    add line info to disassembler

--- xlang/debugger.c
+++ xlang/debugger.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#include "value.h"
 #include "logger.h"
 #include "debugger.h"
 
@@ -7,8 +8,20 @@ static char* LOGNAME = "debugger";
 
 static int simple_instruction(const char* name, int offset)
 {
-    logger(LOGNAME, DEBUG, "got instruction: '%s'", name);
+    logger(LOGNAME, DEBUG, "got instruction: %s", name);
     return offset+1;
+}
+
+static int constant_instruction(const char* name, Bytecode* bytecode, int offset)
+{
+    logger(LOGNAME, DEBUG, "got instruction: %s", name);
+    uint8_t constant = bytecode->code[offset+1];
+    logger(LOGNAME, DEBUG, "constant: %4d", constant);
+    print_value(bytecode->constants.values[constant]);
+    printf("'\n");
+    // note that OP_CONSTANT is two bytes, one 
+    // for opcode and the other for operand
+    return offset + 2;
 }
 
 void disassemble_bytecode(Bytecode* bytecode, const char* name)
@@ -24,9 +37,21 @@ void disassemble_bytecode(Bytecode* bytecode, const char* name)
 int disassemble_instruction(Bytecode* bytecode, int offset)
 {
     logger(LOGNAME, DEBUG, "disassembling instruction at offset: %04d", offset);
+    
+    if ((offset > 0) && (bytecode->lines[offset] == bytecode->lines[offset-1]))
+    {
+       printf("    | "); 
+    }
+    else
+    {
+        printf("%4d", bytecode->lines[offset]);
+    }
+
     uint8_t instruction = bytecode->code[offset];
     switch (instruction)
     {
+        case OP_CONSTANT:
+            return constant_instruction("OP_CONSTANT", bytecode, offset);
         case OP_RETURN:
             return simple_instruction("OP_RETURN", offset);
         default:
