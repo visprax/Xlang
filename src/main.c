3a0fad307e1b048056489a6adf04d56e2ff0ff4d
Saeid Aliei <saeidaliei2019@gmail.com>
2022-09-10 12:25:11 +0430

    add init and free vm functions

--- src/main.c
+++ src/main.c
@@ -2,20 +2,26 @@
 #include "bcstream.h"
 #include "debugger.h"
 #include "logger.h"
+#include "vm.h"
 
 
 int main(int argc, const char* argv[])
 {
+    init_vm();
+
     BCStream bcstream;
     init_bcstream(&bcstream);
     
     int constant = add_constant(&bcstream, 1.5);
     write_bcstream(&bcstream, OP_CONSTANT, 100);
     write_bcstream(&bcstream, constant, 100);
-
     write_bcstream(&bcstream, OP_RETURN, 100);
     
     disassemble_bcstream(&bcstream, "Test Bytecode Stream");
+    
+    interpret(&bcstream);
+
+    free_vm();
 
     free_bcstream(&bcstream);
     return 0;
