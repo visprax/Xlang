29b38f57901c39e2b75ca8908c2bf7cd8808f1c4
Saeid Aliei <saeidaliei2019@gmail.com>
2022-09-19 21:43:17 +0430

    hand-compile test AST to bytecode

--- src/main.c
+++ src/main.c
@@ -4,18 +4,34 @@
 #include "logger.h"
 #include "vm.h"
 
+static char* LOGNAME = "main";
 
 int main(int argc, const char** argv)
 {
+    logger(LOGNAME, INFO, "hand compiling an example AST to bytecode");
+
     init_vm();
 
     BCStream bcstream;
     init_bcstream(&bcstream);
     
-    int constant = add_constant(&bcstream, 1.5);
+    int constant = add_constant(&bcstream, 1.4);
     write_bcstream(&bcstream, OP_CONSTANT, 100);
     write_bcstream(&bcstream, constant, 100);
+        
+    constant = add_constant(&bcstream, 1.6);
+    write_bcstream(&bcstream, OP_CONSTANT, 100);
+    write_bcstream(&bcstream, constant, 100);
+
+    write_bcstream(&bcstream, OP_ADD, 100);
+
+    constant = add_constant(&bcstream, 2.0);
+    write_bcstream(&bcstream, OP_CONSTANT, 100);
+    write_bcstream(&bcstream, constant, 100);
+
+    write_bcstream(&bcstream, OP_DIVIDE, 100);
     write_bcstream(&bcstream, OP_NEGATE, 100);
+
     write_bcstream(&bcstream, OP_RETURN, 100);
     
     disassemble_bcstream(&bcstream, "Test Bytecode Stream");
