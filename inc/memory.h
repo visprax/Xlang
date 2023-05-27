bfa85eacf217e3e08c116b694cbf3aa45c85e993
saliei <saeid.aliei@gmail.com>
2023-05-27 18:20:58 +0200

    fix bugs

--- inc/memory.h
+++ inc/memory.h
@@ -11,6 +11,13 @@
  * before passing `1 + 2` it would be evaluated to 3 and the new capacity 
  * will be 6 but without parentheses it will `1 + 4 = 5`!
  * Here the choice of 8 for initial capacity is arbitrary.
+ * Note that in order to get the O(1) performance when appending an element,
+ * we need to grow the array capacity based on it's old capacity, here we 
+ * do a times 2, we could have also done 1.5x. It may seem that when we
+ * grow the array and copy the old elements to the new one, the complexity 
+ * is O(n), but the copying will happen sometimes and not in all cases, so 
+ * based on amortized analysis, the growing of the array on average is an
+ * O(1), as long as we increase the array capacity as a multiple of the old one.
  */
 #define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity) * 2)
 
--- src/memory.c
+++ src/memory.c
@@ -13,6 +13,13 @@ void* reallocate(void* pointer, size_t old_size, size_t new_size)
         return NULL;
     }
 
+    /*
+     * Note that the malloc maintains the allocated block size 
+     * right before the returned address. If the new size is bigger
+     * than the old size, and there is no immediate block available right 
+     * after the current one, realloc will allocate a complete new block 
+     * and will copy the old block to the new one.
+     */
     void* result = realloc(pointer, new_size);
     if (result == NULL)
     {
