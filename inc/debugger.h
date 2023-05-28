24ba15d9b7f4440df9f121ea670809da6877e3ae
saliei <saeid.aliei@gmail.com>
2023-05-28 12:23:42 +0200

    remove static func declarations

--- inc/debugger.h
+++ inc/debugger.h
@@ -5,7 +5,5 @@
 
 void disassemble_bcstream(BCStream* bcstream, const char* name);
 int disassemble_instruction(BCStream* bcstream, int offset);
-static int simple_instruction(const char* name, int offset);
-static int constant_instruction(const char* name, BCStream* bcstream, int offset);
 
 #endif // XLANG_DEBUGGER_H
