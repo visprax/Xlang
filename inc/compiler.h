b22b67feb774909c504408c07d0b63638968936b
saliei <saeid.aliei@gmail.com>
2023-05-31 08:35:28 +0200

    change compiler signature

--- inc/compiler.h
+++ inc/compiler.h
@@ -1,6 +1,8 @@
 #ifndef XLANG_COMPILER_H
 #define XLANG_COMPILER_H
 
-void compile(const char* source);
+#include "vm.h"
+
+bool compile(const char* source, BCStream* bcstream);
 
 #endif // XLANG_COMPILER_H
--- src/compiler.c
+++ src/compiler.c
@@ -7,25 +7,10 @@
 
 static char* LOGNAME = "compiler";
 
-void compile(const char* source)
+bool compile(const char* source, BCStream* bcstream)
 {
     init_scanner(source);
-    // ask from scanner for a token
-    int line = -1;
-    for(;;)
-    {
-        Token token = scan_token();
-        if(token.line != line)
-        {
-            printf("%4d ", token.line);
-            line = token.line;
-        }
-        else
-        {
-            printf("    | ");
-        }
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
-
-        if(token.type == TOKEN_EOF) break;
-    }
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
 }
